56af65ead82636ed1b17e4a30eddaef3267655e1
Luke <laleggio@ucdavis.edu>
2018-09-05 23:10:10 -0700

    Reworked feedback scripts, starting on point adding

--- routes/feedback/feedback.js
+++ routes/feedback/feedback.js
@@ -5,34 +5,35 @@ var router = express.Router();
 
 var auth = require('../../config/auth');
 
-var profileFeedback = require('../../lib/controllers/feedback/profile-feedback/profile-feedback');
+var addProfileFeedback = require('../../lib/controllers/feedback/profile-feedback/add-profile-feedback');
+var getProfileFeedback = require('../../lib/controllers/feedback/profile-feedback/get-profile-feedback');
 
-router.post('/profile-feedback/add',[auth, profileFeedback.addValidation], function (req, res, next) {
-  profileFeedback.add(req, res, next).then(response => {
+router.post('/profile-feedback/add',[ addProfileFeedback.addValidation], function (req, res, next) {
+  addProfileFeedback.add(req, res, next).then(response => {
     res.json(response);
   }).catch(error => {
     res.json(error)
   });
 });
 
-router.post('/profile-feedback/getSingle', [auth,profileFeedback.getValidation], function (req, res, next) {
-  profileFeedback.getSingle(req, res, next).then(response => {
+router.post('/profile-feedback/getSingle', [auth,getProfileFeedback.getValidation], function (req, res, next) {
+  getProfileFeedback.getSingle(req, res, next).then(response => {
     res.json(response);
   }).catch(error => {
     res.json(error)
   });
 });
 
-router.post('/profile-feedback/getFeedbackAboutMe', [auth,profileFeedback.getFeedbackAboutMeValidation], function (req, res, next) {
-  profileFeedback.getFeedbackAboutMe(req, res, next).then(response => {
+router.post('/profile-feedback/getFeedbackAboutMe', [auth,getProfileFeedback.getFeedbackAboutMeValidation], function (req, res, next) {
+  getProfileFeedback.getFeedbackAboutMe(req, res, next).then(response => {
     res.json(response);
   }).catch(error => {
     res.json(error)
   });
 });
 
-router.post('/profile-feedback/getFeedbackIGave', [auth,profileFeedback.getFeedbackIGaveValidation], function (req, res, next) {
-  profileFeedback.getFeedbackIGave(req, res, next).then(response => {
+router.post('/profile-feedback/getFeedbackIGave', [auth,getProfileFeedback.getFeedbackIGaveValidation], function (req, res, next) {
+  getProfileFeedback.getFeedbackIGave(req, res, next).then(response => {
     res.json(response);
   }).catch(error => {
     res.json(error)
