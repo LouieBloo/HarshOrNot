1824bd9bac033be8e89a21804883cc9a2c5a8f47
Luke <laleggio@ucdavis.edu>
2018-09-08 00:11:34 -0700

    Users can now redeem feedback, feedback properly protected

--- routes/feedback/feedback.js
+++ routes/feedback/feedback.js
@@ -7,8 +7,9 @@ var auth = require('../../config/auth');
 
 var addProfileFeedback = require('../../lib/controllers/feedback/profile-feedback/add-profile-feedback');
 var getProfileFeedback = require('../../lib/controllers/feedback/profile-feedback/get-profile-feedback');
+var unlockProfileFeedback = require('../../lib/controllers/feedback/profile-feedback/unlock-profile-feedback');
 
-router.post('/profile-feedback/add',[ addProfileFeedback.addValidation], function (req, res, next) {
+router.post('/profile-feedback/add',[auth,addProfileFeedback.addValidation], function (req, res, next) {
   addProfileFeedback.add(req, res, next).then(response => {
     res.json(response);
   }).catch(error => {
@@ -39,5 +40,14 @@ router.post('/profile-feedback/getFeedbackIGave', [auth,getProfileFeedback.getFe
     res.json(error)
   });
 });
+
+router.post('/profile-feedback/unlockFeedback', [auth,unlockProfileFeedback.validation], function (req, res, next) {
+  unlockProfileFeedback.unlockProfileFeedback(req, res, next).then(response => {
+    res.json(response);
+  }).catch(error => {
+    res.json(error)
+  });
+});
+
 
 module.exports = router;
