95a629651406aaab8036ec0d2d30c7753c25d4fd
Luke <laleggio@ucdavis.edu>
2018-09-24 21:43:18 -0700

    Added denial and queue fetching for matches

--- routes/users/matches/matches.js
+++ routes/users/matches/matches.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 var getMatches = require('../../../lib/controllers/users/matches/getMatches');
+var getDenials = require('../../../lib/controllers/users/matches/getDenials');
+var getMatchQueue = require('../../../lib/controllers/users/matches/getMatchQueue');
 
 var auth = require('../../../config/auth');
 
@@ -18,5 +20,29 @@ router.post('/getMatches',
   }
 );
 
+router.post('/getDenials',
+  [auth,getDenials.getValidation],
+  function (req, res, next) {
+
+    getDenials.get(req, res, next).then(function (response) {
+      res.json(response);
+    }).catch(function (err) {
+      res.json(err);
+    })
+  }
+);
+
+
+router.post('/getMatchQueue',
+  [auth,getMatchQueue.getValidation],
+  function (req, res, next) {
+
+    getMatchQueue.get(req, res, next).then(function (response) {
+      res.json(response);
+    }).catch(function (err) {
+      res.json(err);
+    })
+  }
+);
 
 module.exports = router;
