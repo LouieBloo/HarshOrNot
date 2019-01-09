5c953c08c675c7aa09b7f546516318a8ed0499a7
Luke <laleggio@ucdavis.edu>
2019-01-08 22:37:34 -0800

    Added route to get channel snippet info

--- routes/users/chat/chat.js
+++ routes/users/chat/chat.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var getToken = require('../../../lib/controllers/chat/createToken');
+var viewChannelInfo = require('../../../lib/controllers/chat/viewChannelInfo');
 
 var auth = require('../../../config/auth');
 
@@ -9,6 +10,17 @@ router.post('/getToken',
   [auth,getToken.validation],
   function (req, res, next) {
     getToken.generateToken(req, res, next).then(function (response) {
+      res.json(response);
+    }).catch(function (err) {
+      res.json(err);
+    })
+  }
+);
+
+router.post('/viewChannelInfo',
+  [auth,viewChannelInfo.validation],
+  function (req, res, next) {
+    viewChannelInfo.viewChannelInfo(req, res, next).then(function (response) {
       res.json(response);
     }).catch(function (err) {
       res.json(err);
